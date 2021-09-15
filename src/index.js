339265f7094548a90b5a3377b85ee3bf9c7a5139
theo.clapperton <theo.clapperton@timpson.com>
2021-09-15 16:31:19 +0100

    Init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './css/index.css';
+
+import App from './screens/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/screens/App.js
@@ -0,0 +1,11 @@
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello world!
+    </div>
+  );
+}
+
+export default App;
