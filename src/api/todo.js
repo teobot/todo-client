24faa111383c534f996ea95f83dbfadf1394f9fa
teobot <theoclapperton@outlook.com>
2021-09-19 22:15:41 +0100

    Update api link

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+let DEV = false;
+
 const AxiosInstance = () => {
   const instance = axios.create({
-    baseURL: "http://localhost:3333/api/v1",
+    baseURL: DEV ? "http://localhost:3333/api/v1" : "https://teobot-todo-api.herokuapp.com/api/v1",
     timeout: 5000,
   });
 
